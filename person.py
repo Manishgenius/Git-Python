dc6864f7d272db8fa9f56896fd4c7780eee9a2ae
Manish Pandey <mnshkmr@gmail.com>
2020-03-16 12:37:24 +0530

    Version 3

--- person.py
+++ person.py
@@ -19,7 +19,13 @@ class Employee(Person):
     # Here we return true 
     def isEmployee(self): 
         return True
-   
+class Student:
+    def __init__(self,name,address):
+        self.name=name
+        self.address=address
+ 
+student1 =Student("manish","Ballia")
+print(student1.name)          
 # Driver code 
 emp = Person("Geek1")  # An Object of Person 
 print(emp.getName(), emp.isEmployee()) 
